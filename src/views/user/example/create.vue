f76c2e92ffbf7e1e839ab40d039714754ebc2809
orzorzooo <orzorzooo@gmail.com>
2023-01-18 12:40:15 +0800

    EDIT: example delete方法

--- src/views/user/example/create.vue
+++ src/views/user/example/create.vue
@@ -1,6 +1,48 @@
 <template>
-  <!-- <orzHeader></orzHeader> -->
+  <div class="text-purple-600">
+    <q-form class="q-gutter-md text-purple-600">
+      <q-toggle
+        :label="data.status == 'draft' ? '草稿' : '發佈'"
+        v-model="data.status"
+        color="accent"
+        false-value="draft"
+        true-value="published"
+        size="lg"
+      />
+      <div>Name</div>
+      <q-input
+        filled
+        v-model="data.name"
+        ref="nameRef"
+        :rules="[(val) => !!val || '必填項目']"
+      ></q-input>
+      <div>Description</div>
+      <q-input filled type="textarea" v-model="data.description"></q-input>
+    </q-form>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn round icon="check" size="lg" color="accent" @click="submit" />
+    </q-page-sticky>
+  </div>
 </template>
 <script setup>
-import orzHeader from "@/layouts/user/components/header.vue";
+import { get, post } from "@/api/request";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const collection = "example";
+const nameRef = ref(null);
+const router = useRouter();
+const data = ref({
+  status: "draft",
+  name: "",
+  description: "",
+});
+async function submit() {
+  nameRef.value.validate();
+  if (nameRef.value.hasError) return;
+  const res = await post({ collection, data: data.value });
+  if (res) {
+    return router.push({ name: "Example" });
+  }
+}
 </script>
--- src/views/user/example/index.vue
+++ src/views/user/example/index.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
-    <orzHeader>
-      <div class="text-lg font-bold uppercase">
-        {{ items ? items.meta.total_count : 0 }} Items
-      </div>
-      <q-btn
-        flat
-        round
-        dense
-        icon="delete_outline"
-        size="lg"
-        v-if="selected.length"
-      />
-      <q-btn
-        flat
-        round
-        dense
-        icon="add_circle"
-        size="lg"
-        @click="router.push({ name: 'ExampleCreate' })"
-      />
-    </orzHeader>
+    <div class="text-purple-600 mb-2">
+      <q-toolbar>
+        <q-toolbar-title
+          ><div class="text-lg font-bold uppercase">
+            {{ items ? items.meta.total_count : 0 }} Items
+          </div></q-toolbar-title
+        >
+
+        <q-btn
+          round
+          icon="delete_outline"
+          color="red"
+          v-if="selected.length"
+          class="mr-3"
+          @click="onDelete"
+        />
+        <q-btn
+          round
+          icon="add"
+          color="accent"
+          @click="router.push({ name: 'ExampleCreate' })"
+        />
+      </q-toolbar>
+    </div>
     <q-table
       flat
       :rows="items ? items.data : []"
@@ -34,7 +37,7 @@
   </div>
 </template>
 <script setup>
-import { get } from "@/api/request";
+import { get, del } from "@/api/request";
 import { onMounted, ref } from "vue";
 import { date } from "quasar";
 import { useRouter } from "vue-router";
@@ -47,7 +50,6 @@ const items = ref(null);
 const loading = ref(true);
 const cols = ref([
   { name: "name", label: "name", field: "name", align: "left" },
-  // { name: "mainmeal", label: "餐點名稱", field: "mainmeal", align: "left" },
   {
     name: "date_created",
     label: "建立日期",
@@ -58,10 +60,22 @@ const cols = ref([
 ]);
 const selected = ref([]);
 onMounted(async () => {
+  await init();
+});
+
+async function init() {
+  selected.value = [];
   const res = await get({ collection, params: { meta: "total_count" } });
   items.value = res;
-});
+}
+
 function onRowClick(e, row, i) {
   console.log(row.id);
+}
+
+async function onDelete() {
+  const ids = selected.value.map((v) => v.id);
+  const res = await del({ ids, collection });
+  if (res) return init();
 }
 </script>
